122c71fac2c59db4b2d676a4ced5cd14a470ed87
Saran K <hiclashersaran@gmail.com>
2026-08-25 10:45:28 +0530

    Update username in update-badge-script.js

--- update-badge-script.js
+++ update-badge-script.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
 
-let userName = "saran-k-07";
+let userName = "Sanjay016-06";
 
 let pipelineRun = true; // Set to false for local debugging
 let templateFile = 'github-repo-count-template.svg';
